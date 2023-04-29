12705298776898e71554df9209653a0c5ee3c43e
Catalina Restrepo Salgado <rposalgadoc@gmail.com>
2023-04-29 13:38:20 -0500

    Change ground check radius

--- Final/Assets/Scripts/eggs.cs
+++ Final/Assets/Scripts/eggs.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class eggs: MonoBehaviour
+public class Eggs: MonoBehaviour
 {
     [SerializeField] private Player player;
 
