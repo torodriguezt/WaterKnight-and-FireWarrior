3e2fc532fc5c0678569f21a6e2898eb6e801982d
caroAM22 <caalvarezmu@unal.edu.co>
2023-06-21 16:22:20 -0500

    Muted button

--- Final/Assets/Scripts/Scenes/HUDpaused.cs
+++ Final/Assets/Scripts/Scenes/HUDpaused.cs
@@ -9,12 +9,47 @@ public class HUDpaused : MonoBehaviour
 {
     [SerializeField]
     private Button _backButton;
+    [SerializeField]
+    private AudioSource audioSource;
+    [SerializeField]
+    private Button muteButton;
+    [SerializeField]
+    private Sprite mutedSprite;
+    [SerializeField]
+    private Sprite unmutedSprite;
+
+    private bool isMuted = false;
+    private Image buttonImage;
     private void Start()
     {
+        buttonImage = muteButton.GetComponent<Image>();
+        muteButton.onClick.AddListener(ToggleMute);
+        UpdateButtonImage();
         _backButton.onClick.AddListener(OnButtonBack);
     }
     void OnButtonBack()
     {
         GameManager.Instance.GameOver();
     }
+
+    void ToggleMute()
+    {
+        isMuted = !isMuted;
+        audioSource.mute = isMuted;
+        UpdateButtonImage();
+    }
+
+    void UpdateButtonImage()
+    {
+        if (isMuted)
+        {
+            buttonImage.sprite = mutedSprite;
+        }
+        else
+        {
+            buttonImage.sprite = unmutedSprite;
+        }
+    }
 }
+
+
